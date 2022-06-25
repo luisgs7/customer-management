f836e1fbb6dc776074c4612aaf0417ca3095ca93
Luís Gustavo <luisgsilva270@gmail.com>
2022-06-25 17:46:47 +0000

    add staticfiles folder

--- customer_management/settings.py
+++ customer_management/settings.py
@@ -129,6 +129,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = 'media'

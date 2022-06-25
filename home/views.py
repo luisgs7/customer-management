e4e6d78f72e70e4def12f7252b08344ed6cb9589
Luís Gustavo <luisgsilva270@gmail.com>
2022-06-25 13:38:48 +0000

    new view my_logout

--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
 
 def home(request):
+    return render(request, 'home.html')
+
+
+def my_logout(request):
+    logout(request)
     return render(request, 'home.html')

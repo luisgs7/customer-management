02142ad75ce1e9f00d44084fbd20701c65dd27e3
Luís Gustavo <luisgsilva270@gmail.com>
2022-06-25 14:34:47 +0000

    config static

--- customer_management/settings.py
+++ customer_management/settings.py
@@ -122,12 +122,13 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [
-    'statics',
-]
-
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 MEDIA_URL = '/media/'
 

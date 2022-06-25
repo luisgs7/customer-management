77e65a1fc789834f6202f0d387bef63fb5b187e3
Luís Gustavo <luisgsilva270@gmail.com>
2022-06-25 18:20:48 +0000

    config template login

--- customer_management/settings.py
+++ customer_management/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'customer_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

a4a997f19f93dc909b5e13c442596a2d9d792287
Luís Gustavo <luisgsilva270@gmail.com>
2022-06-25 12:49:27 +0000

    config STATICFILES_DIRS and MEDIA

--- customer_management/settings.py
+++ customer_management/settings.py
@@ -121,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    'static'
+]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = 'media'

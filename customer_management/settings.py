ad845dbd4116e9730f6753f7914d7aa800a11a91
Luís Gustavo <luisgsilva270@gmail.com>
2022-06-25 14:51:09 +0000

    config staticfiles

--- customer_management/settings.py
+++ customer_management/settings.py
@@ -124,13 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'

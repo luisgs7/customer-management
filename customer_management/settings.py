e9c954e5379a38a975f8bd1befd236247820c0b4
Luís Gustavo <luisgsilva270@gmail.com>
2022-06-25 18:26:38 +0000

    refactor static url

--- customer_management/settings.py
+++ customer_management/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_URL = (os.path.join(BASE_DIR, '/clients/templates/static/'))
+STATIC_URL = (os.path.join(BASE_DIR, 'clients/templates/static/'))
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'clients/templates/'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
 

88620a004499bad800118fba5434d3eba82fa573
Luís Gustavo <luisgsilva270@gmail.com>
2022-06-25 12:49:01 +0000

    config static files

--- customer_management/urls.py
+++ customer_management/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from clients import urls as clients_urls
 
 urlpatterns = [
     path('clients/', include(clients_urls)),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
